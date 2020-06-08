5bdaf26e7c542d7397890678afbf2492cf524f75
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-06-07 23:54:26 -0400

    Create 09.ImplementQueueWith2Stack.py

---
+++ 09.ImplementQueueWith2Stack.py
@@ -0,0 +1,29 @@
+class CQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+
+    def appendTail(self, value: int) -> None:
+        self.stack1.append(value)
+
+
+    def deleteHead(self) -> int:
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            if not self.stack1:
+                return -1
+            while self.stack1:
+                value = self.stack1.pop()
+                self.stack2.append(value)
+            return self.stack2.pop()    
+
+
+
+
+# Your CQueue object will be instantiated and called as such:
+# obj = CQueue()
+# obj.appendTail(value)
+# param_2 = obj.deleteHead()
